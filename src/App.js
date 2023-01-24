4693250c2bfb23970ddab47af4539100826366dc
cgargatagli <ciroshaila@gmail.com>
2023-01-24 20:21:28 -0300

    MVP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import logo from './logo.svg';
+import Main from './pages/Main'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Los 15 de Ciara
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main />
   );
 }
 
